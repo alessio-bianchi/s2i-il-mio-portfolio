dcddc60e85c73c6a59f5b319ad6c81d8392c86d8
alessio-bianchi <88654838+alessio-bianchi@users.noreply.github.com>
2021-08-18 14:55:04 +0200

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,41 @@
+// PROGRESS BAR 
+
+
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+// HEADER APERTURA E CHIUSURA MENU' E HAMBURGER X QUANDO CHIUSO 
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+  
+  function closeMenu() {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+  }
+  
+// CAROSELLO
+
+$(document).ready(function(){
+  // Activate Carousel
+  $("#carouselExampleIndicators").carousel({interval: 10000, pause: "hover"});
+});
+
